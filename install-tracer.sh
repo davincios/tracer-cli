6b69ca0fa0a86b83c3c96eebc2f4f7adb3331b88
Vincent Hus <jvf.hus@gmail.com>
2024-04-30 03:02:36 +0100

    update install

--- install-tracer.sh
+++ install-tracer.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 # Check if an API key was provided
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ] || [ "$1" != "--api-key" ]; then
     echo "Usage: $0 --api-key <API_KEY>"
     exit 1
 fi
 
-API_KEY=${1/--api-key=/}
+API_KEY=$2
 
 # Define the version of the tracer you want to download
-TRACER_VERSION="v0.0.22"
+TRACER_VERSION="v0.0.23"
 TRACER_URL="https://github.com/davincios/tracer-cli/releases/download/${TRACER_VERSION}/tracer-x86_64-unknown-linux-gnu.tar.gz"
+
+# Check for wget, use curl if not available
+DOWNLOAD_COMMAND="wget"
+command -v wget >/dev/null 2>&1 || DOWNLOAD_COMMAND="curl -L -O"
 
 # Download the latest release
 echo "Downloading Tracer CLI version ${TRACER_VERSION}..."
-wget $TRACER_URL
+$DOWNLOAD_COMMAND "$TRACER_URL"
 
 # Extract the downloaded tarball
 echo "Extracting Tracer CLI..."
@@ -22,20 +26,31 @@ tar -xzf tracer-x86_64-unknown-linux-gnu.tar.gz
 
 # Execute setup with the API key
 echo "Setting up Tracer CLI with provided API key..."
-./tracer setup $API_KEY
+./tracer setup "$API_KEY"
 
 # Create directory /etc/tracer if it doesn't exist and copy the binary there
 sudo mkdir -p /etc/tracer/
 sudo cp tracer /etc/tracer/tracer
 
-# Add /etc/tracer to the PATH in .bashrc if not already present
-if ! grep -q '/etc/tracer' ~/.bashrc; then
-    echo 'export PATH="$PATH:/etc/tracer"' >>~/.bashrc
-    echo 'alias tracer="tracer"' >>~/.bashrc
+# Determine the shell and select the appropriate config file
+if [ -n "$ZSH_VERSION" ]; then
+    CONFIG_FILE="$HOME/.zshrc"
+elif [ -n "$BASH_VERSION" ]; then
+    CONFIG_FILE="$HOME/.bashrc"
+else
+    echo "Unsupported shell. Please add Tracer CLI to your PATH manually."
+    exit 1
 fi
 
-# Apply changes in .bashrc
-source ~/.bashrc
+# Add /etc/tracer to the PATH in the appropriate config file if not already present
+if ! grep -q '/etc/tracer' "$CONFIG_FILE"; then
+    echo 'export PATH="$PATH:/etc/tracer"' >>"$CONFIG_FILE"
+    echo 'alias tracer="tracer"' >>"$CONFIG_FILE"
+    source "$CONFIG_FILE"
+fi
+
+# Note for user to source the config file
+echo "Please source your $CONFIG_FILE or open a new terminal session to complete setup."
 
 # Check tracer version to confirm installation
 tracer --version

98be94400faf8900aec294674e7e2ccfba6c682c
Vincent Hus <jvf.hus@gmail.com>
2024-04-30 20:15:59 +0100

    tracer version update in the shell script

--- install-tracer.sh
+++ install-tracer.sh
@@ -9,7 +9,7 @@ fi
 API_KEY=$2
 
 # Define the version of the tracer you want to download
-TRACER_VERSION="v0.0.32"
+TRACER_VERSION="v0.0.47"
 TRACER_LINUX_URL="https://github.com/davincios/tracer-cli/releases/download/${TRACER_VERSION}/tracer-x86_64-unknown-linux-gnu.tar.gz"
 TRACER_MACOS_AARCH_URL="https://github.com/davincios/tracer-cli/releases/download/v0.0.32/tracer-aarch64-apple-darwin.tar.gz"
 TRACER_MACOS_UNIVERSAL_URL="https://github.com/davincios/tracer-cli/releases/download/${TRACER_VERSION}/tracer-universal-apple-darwin.tar.gz"
